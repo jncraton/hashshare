432967a081f21c857a2ffb46bf3283cc17a1c8ba
jncraton <jncraton@gmail.com>
2025-11-28 11:18:17 -0500

    Add autoshare.js

---
+++ autoshare.js
@@ -0,0 +1,24 @@
+(() => {
+  // Load inital data if present
+  let data = {}
+
+  try {
+    data = JSON.parse(atob(location.hash.slice(1)))
+  } catch (e) {
+    console.warn(e)
+  }
+
+  const setData = (id, value) => {
+    // Set data and update URL hash
+    data[id] = value
+    location.hash = btoa(JSON.stringify(data))
+  }
+    
+  document.querySelectorAll('[autoshare-value]').forEach(el => {
+    el.value = data[el.id] ?? ''
+
+    el.addEventListener('input', () => {
+      setData(el.id, el.value)
+    })
+  })
+})()
