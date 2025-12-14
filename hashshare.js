b3d5e586080c191fd70fc720f0724cbfc9f51cae
Jon Craton <jncraton@gmail.com>
2025-12-13 20:10:48 -0500

    Automatically detect element types

--- hashshare.js
+++ hashshare.js
@@ -14,17 +14,21 @@
     location.hash = btoa(JSON.stringify(data))
   }
 
-  document.querySelectorAll('[hashshare-value]').forEach(el => {
-    if (data[el.id]) {
-      // Set the element from the provided value in the hash
-      el.value = data[el.id]
-      // Manually trigger an input event as if a user has entered data
-      // into this field
-      el.dispatchEvent(new Event('input', { bubbles: true }))
-    }
+  document.querySelectorAll('[hashshare]').forEach(el => {
+    // If this is an input of some form (input, textarea, etc) then
+    if ('value' in el) {
+      if (data[el.id]) {
+        // Set the element from the provided value in the hash
+        el.value = data[el.id]
+        // Manually trigger an input event as if a user has entered data
+        // into this field
+        el.dispatchEvent(new Event('input', { bubbles: true }))
+      }
 
-    el.addEventListener('input', () => {
-      setData(el.id, el.value)
-    })
+      // Update the URL hash when this input changes
+      el.addEventListener('input', () => {
+        setData(el.id, el.value)
+      })
+    }
   })
 })()
