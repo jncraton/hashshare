12cb0fb276eddb63c88dd4d7e1fea45ac5f1a062
Jon Craton <jncraton@gmail.com>
2025-12-13 19:59:55 -0500

    Trigger input events automatically

--- hashshare.js
+++ hashshare.js
@@ -15,7 +15,13 @@
   }
 
   document.querySelectorAll('[hashshare-value]').forEach(el => {
-    el.value = data[el.id] ?? ''
+    if (data[el.id]) {
+      // Set the element from the provided value in the hash
+      el.value = data[el.id]
+      // Manually trigger an input event as if a user has entered data
+      // into this field
+      el.dispatchEvent(new Event('input', { bubbles: true }))
+    }
 
     el.addEventListener('input', () => {
       setData(el.id, el.value)
