d0d7a197423b30691ab45625ea143fd1262a4d7a
Jon Craton <jncraton@gmail.com>
2025-12-13 22:43:41 -0500

    Add test for deflate compression

--- test_example.py
+++ test_example.py
@@ -18,6 +18,10 @@ def test_page_content(page: Page):
     page.goto(f"{file_url}#eyJ0ZXh0IjoiSGVsbG8sIHdvcmxkIn0=")
     expect(page.locator('textarea')).to_have_value("Hello, world")
 
+def test_page_content_deflate(page: Page):
+    page.goto(f"{file_url}#;q1YqSa0oUbJS8kjNycnXUSjPL8pJUaoFAA==")
+    expect(page.locator('textarea')).to_have_value("Hello, world")
+
 def test_page_url(root):
     root.locator('textarea').fill('Hello, world')
     expect(root).to_have_url(re.compile(".*eyJ0ZXh0IjoiSGVsbG8sIHdvcmxkIn0="))
